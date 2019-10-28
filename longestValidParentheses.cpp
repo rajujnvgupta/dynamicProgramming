07672e9ab06eba96c93d3a20aae4b1b13d6495a1
rajujnvgupta <39230239+rajujnvgupta@users.noreply.github.com>
2019-10-28 14:43:25 +0530

    first commit

---
+++ longestValidParentheses.cpp
@@ -0,0 +1,40 @@
+/*
+    @rajujnvgupta  
+    28/oct/2019
+*/
+#include <bits/stdc++.h>
+using namespace std;
+string str;
+int longestValidParentheses(string s) {
+        
+        int ans = 0;
+        stack<int>st;
+        
+        st.push(0);
+        for(int i = 0; i < s.size(); i++){
+            if(s[i]=='('){
+                st.push(i+1);
+            }
+            else if(s[i]==')'){
+                
+                if(st.size() == 1){
+                    st.pop();
+                    st.push(i+1);
+                }
+                else{
+                    
+                    st.pop();
+                    int tp = st.top();
+                    ans = max(ans, i+1 - tp);    
+                }
+            }
+        }
+        return ans;
+    }
+
+int main(){
+    ///enter string having char '(' or ')'
+    cin>>str;
+    cout<<solve(str)<<endl;
+}
+
